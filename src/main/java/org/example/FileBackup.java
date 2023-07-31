0cbe7d086f25d1666cdd51503511ca7a02396cdb
Sakaev Ruslan <hardist@mail.ru>
2023-07-31 23:30:28 +0300

    Добавил решение задач

---
+++ src/main/java/org/example/FileBackup.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class FileBackup {
+    public static void createBackup(String sourceDirectory) {
+        File sourceDir = new File(sourceDirectory);
+        File backupDir = new File(sourceDir.getParent(), "backup");
+
+        if (!backupDir.exists()) {
+            backupDir.mkdir();
+        }
+
+        File[] files = sourceDir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    File destination = new File(backupDir.getAbsolutePath(), file.getName());
+                    try {
+                        Files.copy(file.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        System.err.println("Не удалось выполнить резервное копирование " + file.getName() + ": " + e.getMessage());
+                    }
+                }
+            }
+            System.out.println("Резервное копирование успешно завершено.");
+        }
+    }
+
+    public static void main(String[] args) {
+        String sourceDirectory = "src/main/java/resources"; 
+        createBackup(sourceDirectory);
+    }
+}
---
+++ src/main/java/org/example/TicTacToe.java
@@ -0,0 +1,81 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TicTacToe {
+    private static final int SIZE = 3;
+
+    public static void saveGameBoard(int[][] gameBoard, String filePath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (int row = 0; row < SIZE; row++) {
+                for (int col = 0; col < SIZE; col++) {
+                    writer.write(String.valueOf(gameBoard[row][col]));
+                }
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при сохранении игрового поля: " + e.getMessage());
+        }
+    }
+
+    public static int[][] loadGameBoard(String filePath) {
+        int[][] gameBoard = new int[SIZE][SIZE];
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            for (int row = 0; row < SIZE; row++) {
+                String line = reader.readLine();
+                for (int col = 0; col < SIZE; col++) {
+                    gameBoard[row][col] = Character.getNumericValue(line.charAt(col));
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при загрузке игрового поля: " + e.getMessage());
+        }
+        return gameBoard;
+    }
+
+    public static void printGameBoard(int[][] gameBoard) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                switch (gameBoard[row][col]) {
+                    case 0:
+                        System.out.print("• ");
+                        break;
+                    case 1:
+                        System.out.print("X ");
+                        break;
+                    case 2:
+                        System.out.print("O ");
+                        break;
+                    default:
+                        System.out.print("? ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] gameBoard = {
+                {1, 0, 2},
+                {0, 1, 0},
+                {2, 0, 1}
+        };
+
+        String filePath = "src/main/java/resources/gameboard.txt";
+
+        saveGameBoard(gameBoard, filePath);
+
+        int[][] loadedGameBoard = loadGameBoard(filePath);
+
+        System.out.println("Игровое поле:");
+        printGameBoard(gameBoard);
+
+        System.out.println("\nЗагрузка игрового поля:");
+        printGameBoard(loadedGameBoard);
+    }
+}
+
